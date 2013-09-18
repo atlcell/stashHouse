f4895a4da05768ae3c1d689440079552661e21cc
marcell <marcell@tisdol.com>
2013-09-18 19:27:18 -0400

    Public
    Public

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-   _____ _            _     _    _                            _     
-  / ____| |          | |   | |  | |                          | |    
- | (___ | |_ __ _ ___| |__ | |__| | ___  _   _ ___  ___   ___| |__  
-  \___ \| __/ _` / __| '_ \|  __  |/ _ \| | | / __|/ _ \ / __| '_ \ 
-  ____) | || (_| \__ \ | | | |  | | (_) | |_| \__ \  __/_\__ \ | | |
- |_____/ \__\__,_|___/_| |_|_|  |_|\___/ \__,_|___/\___(_)___/_| |_|
+#StashHouse.sh
+StashHouse.sh is a basic and simple script for automatically backing up websites and applications hosted on a 
+Media Temple Dedicated Virtual (DV) server.
+
+It is dependent upon the very good [s3sync](https://github.com/ms4720/s3sync) library.
+
+At eyespeak, we use a custom and more detailed version that dumps our databases indivudally as we host *ALOT* of websites.
+
+To install, you simply need to drag and drop the whole repo into your servers root, *NOT THE ROOT FOLDER*.
+
+1. Change the Secret Key information located in /etc/s3conf/s3config.yml
+2. Change the Bucket and Key to your own located on amazon s3
+
+For more information and and more complexity, reference the s3sync documentation.
--- StashHouse.sh
+++ StashHouse.sh
@@ -7,14 +7,18 @@
 # 2013 Marcell Tisdol | http://tisdol.com | http://eyespeak.com     
 # Version 1.0/ NoSQL   
 
+#File Backup
 
 #Are we in root? No?
-cd /                                                                                                
+cd /                                                    
 # Make our work Folders
+mkdir /dbs3/
 mkdir /backup/
 mkdir /zips/
+
 #Copy Everything from /vhosts/ to our work directory while excluding certain culprits
 rsync -av --progress --exclude-from '/exclude-list.txt' /var/www/vhosts/ /backup/
+
 #For Every directory, zip httpdocs and move it to the root of our work directory
 /bin/echo
 for dir in `/bin/ls /backup/`;
@@ -25,9 +29,23 @@ do
 	cd /backup
 done
 /bin/echo
+
+#SQL Backup
+
+#Mount the Plesk Password
+PASS="`cat /etc/psa/.psa.shadow` "
+
+#Dump That File
+mysqldump -u admin -p$PASS --all-databases > /all-database.sql
+/bin/echo
+mv /all-database.sql /dbs3
+
 #Let's move /zips/ to s3
 cd /
-ruby /s3sync/s3sync.rb  -r  /zips/  three_es:sites
+ruby /s3sync/s3sync.rb  -r  /zips/  bucket:key 
+ruby /s3sync/s3sync.rb  -r  /database/  bucket:key 
+
 /bin/echo
+/bin/rm -rf /dbs3/
 /bin/rm -rf /backup/
 /bin/rm -rf /zips/
